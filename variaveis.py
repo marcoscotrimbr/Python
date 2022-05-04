8fca507ab75641b8a4f6b4250cab9a094e8ca12a
Marcos Cotrim <94663094+marcoscotrimbr@users.noreply.github.com>
2022-05-03 23:19:48 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,7 @@
+name="Humberto Delegado"
+empresa='FIAP'
+qtd_funcionarios=800
+mediaMensalidade=856.50
+print(name + " trabalha na empresa " + empresa)
+print("A empresa tem", qtd_funcionarios, "funcionarios")
+print("A mensalidade custa na média " + str(mediaMensalidade))
